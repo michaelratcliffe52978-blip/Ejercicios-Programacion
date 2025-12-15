e493d9795f75821d6601a8f0cf739f10e07e81b3
Michael <michael.ratcliffe.52978@ikasle.egibide.org>
2025-12-15 12:30:54 +0100

    P3e4

---
+++ P3e4/src/ExcepcionContacto.java
@@ -0,0 +1,2 @@
+public class ExcepcionContacto extends Exception {
+}
--- P3e4/src/Main.java
+++ P3e4/src/Main.java
@@ -1,15 +1,92 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+    private static Map<String, Integer> map = new HashMap<>();
+    public static void main(String[] args) {
+        try {
+            int opcion;
+            do {
+                menu();
+                opcion = sc.nextInt();
+                switch (opcion) {
+                    case 1:
+                        opcion1();
+                        break;
+                    case 2:
+                        opcion2();
+                        break;
+                    case 3:
+                        opcion3();
+                        break;
+                    case 4:
+                        opcion4();
+                        break;
+                    case 5:
+                        opcion5();
+                        break;
+                    case 6:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default: System.out.println("Opción no valida. Por favor, elija una opción entre a y f.");
+                }
+            }while(opcion != 6);
+        }catch (Exception e){
+            System.err.println(e.getMessage());
         }
+
     }
+    public static void menu() {
+        System.out.println("\n--- M E N Ú ---");
+        System.out.println("""
+                1.- Añadir un nuevo número.
+                2.- Borrar un número.
+                3.- Buscar un número.
+                4.- Borrar por nombre.
+                5.- Buscar por nombre.
+                6.- Salir.""");
+        System.out.print("Seleccione una opción (1–6): ");
+    }
+
+    public static void opcion1(){
+        String siNo = "";
+        do {
+            
+            try {
+                System.out.println("\nNUEVO CONTACTO: ");
+                System.out.print("Nombre: ");
+                sc.nextLine();
+                String nombre = sc.nextLine().trim();
+                
+                if(nombre.isEmpty()){
+                    throw new ExcepcionContacto();
+                }
+                
+                System.out.print("Número de teléfono: ");
+                int tel = sc.nextInt();
+
+                map.put(nombre, tel);
+                System.out.println("Contacto añadido correctamente.");
+
+
+                System.out.println("¿Quieres añadir otro contacto? (si/no)");
+                siNo = sc.next().trim().toLowerCase();
+            }catch (ExcepcionContacto e){
+                System.out.println("Error al intentar ingresar el contacto.");
+            }
+        }while (siNo.equalsIgnoreCase("si")||siNo.equalsIgnoreCase("s") );
+        
+    }
+    public static void opcion2(){
+    }
+    public static void opcion3(){
+    }
+    public static void opcion4(){
+    }
+    public static void opcion5(){
+
+    }
+
+
+
 }
