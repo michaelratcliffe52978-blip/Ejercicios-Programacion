ace67326e32e8f58808a825ab268057b4d433a43
Michael <michael.ratcliffe.52978@ikasle.egibide.org>
2025-12-16 09:10:54 +0100

    P3e4

--- P3e4/src/Main.java
+++ P3e4/src/Main.java
@@ -1,8 +1,12 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
     private static Map<String, Integer> map = new HashMap<>();
+    private static String nombre;
+    private static Integer tel;
     public static void main(String[] args) {
         try {
             int opcion;
@@ -51,19 +55,31 @@ public class Main {
     public static void opcion1(){
         String siNo = "";
         do {
-            
             try {
                 System.out.println("\nNUEVO CONTACTO: ");
                 System.out.print("Nombre: ");
                 sc.nextLine();
-                String nombre = sc.nextLine().trim();
+                nombre = sc.nextLine().trim();
                 
                 if(nombre.isEmpty()){
                     throw new ExcepcionContacto();
                 }
-                
-                System.out.print("Número de teléfono: ");
-                int tel = sc.nextInt();
+
+                boolean errorNumero = true;
+                do {
+                    try {
+                        System.out.print("Número de teléfono (9 nº): ");
+                        tel = sc.nextInt();
+
+                        if(tel < 100000000 || tel > 999999999){
+                            throw new ExcepcionContacto();
+                        }
+                        errorNumero = false;
+                    }catch (Exception e){
+                        System.out.println("Error, introduce un nº de teléfono de 9 dígitos.");
+                        sc.nextLine();
+                    }
+                }while (errorNumero);
 
                 map.put(nombre, tel);
                 System.out.println("Contacto añadido correctamente.");
@@ -78,12 +94,56 @@ public class Main {
         
     }
     public static void opcion2(){
+        System.out.print("Introduce el nº de teléfono y el contacto será BORRADO: ");
+        sc.nextLine();
+        int telBorrar = sc.nextInt();
+
+        boolean borrado = map.entrySet().removeIf(
+                entry -> entry.getValue().equals(telBorrar)
+        );
+
+        if(borrado){
+            System.out.println("El teléfono '" + telBorrar + "' se ha borrado correctamente.");
+        }else{
+            System.out.println("No existe ningún contacto con ese teléfono.");
+        }
     }
     public static void opcion3(){
+        System.out.print("Introduce el nº de teléfono y buscaremos el contacto: ");
+        sc.nextLine();
+        int telBuscar = sc.nextInt();
+
+        if(map.containsValue(telBuscar)){
+            System.out.println("El teléfono '" + telBuscar + "' se ha encontrado correctamente.");
+        }else{
+            System.out.println("No existe ningún contacto con ese teléfono.");
+        }
+
     }
     public static void opcion4(){
+        System.out.print("Introduce el nombre y borraremos el contacto: ");
+        sc.nextLine();
+        String nombreBuscar = sc.nextLine().trim();
+        if(map.containsKey(nombreBuscar)){
+            map.remove(nombreBuscar);
+            System.out.print("El contacto '"+nombreBuscar+"' se ha borrado correctamente.");
+
+        }else {
+            System.out.println("No existe el contacto con el nombre '" + nombreBuscar + "'");
+        }
+
     }
     public static void opcion5(){
+        System.out.print("Introduce el nombre y buscaremos el contacto: ");
+        sc.nextLine();
+        String nombreBuscar = sc.nextLine();
+
+        if(map.containsKey(nombreBuscar)){
+            System.out.println("El contacto '" + nombreBuscar + "' se ha encontrado correctamente.");
+        }else{
+            System.out.println("No existe ningún contacto con ese nombre.");
+        }
+
 
     }
 
